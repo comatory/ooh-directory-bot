5796e261ac05023fdd062056324eb49fb7504f12
comatory <ondrej.synacek@productboard.com>
2023-12-22 15:39:02 +0100

    refactor scraping functions

--- scraper.go
+++ scraper.go
@@ -7,27 +7,35 @@ import (
 	"net/http"
 )
 
-func RequestRandomsHtmlBody(url string, client http.Client) (string, error) {
+func prepareRequest(url string) (*http.Request, error) {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	req.Header.Set("User-Agent", "ooh-directory-random-bot")
 	req.Header.Set("Accept-Language", "en-us, en-gb, en")
 	req.Header.Set("Accept", "text/html")
 
+	return req, nil
+}
+
+func performRequest(req *http.Request, client *http.Client) (*http.Response, error) {
 	res, err := client.Do(req)
 
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return "", errors.New(fmt.Sprintf("Request failed: %d", res.StatusCode))
+		return nil, errors.New(fmt.Sprintf("Request failed: %d", res.StatusCode))
 	}
 
+	return res, nil
+}
+
+func readResponseBody(res *http.Response) (string, error) {
 	bytes, err := io.ReadAll(res.Body)
 
 	if err != nil {
@@ -38,5 +46,23 @@ func RequestRandomsHtmlBody(url string, client http.Client) (string, error) {
 }
 
 func ScrapeRandom(url string, client http.Client) (string, error) {
-	return RequestRandomsHtmlBody(url, client)
+	req, prepareErr := prepareRequest(url)
+
+	if prepareErr != nil {
+		return "", prepareErr
+	}
+
+	res, requestError := performRequest(req, &client)
+
+	if requestError != nil {
+		return "", requestError
+	}
+
+	body, readError := readResponseBody(res)
+
+	if readError != nil {
+		return "", readError
+	}
+
+	return body, nil
 }
